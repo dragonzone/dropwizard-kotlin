0ae6c218baa559aad5ce266dd3ccdbc3d016ca85
Bryan Harclerode <baharclerode@devdragon.net>
2023-07-22 20:57:38 -0500

    Update copyright

--- src/main/kotlin/zone/dragon/dropwizard/kotlin/coroutines/scopes/ApplicationCoroutineScope.kt
+++ src/main/kotlin/zone/dragon/dropwizard/kotlin/coroutines/scopes/ApplicationCoroutineScope.kt
@@ -1,18 +1,39 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019-2023 Bryan Harclerode
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package zone.dragon.dropwizard.kotlin.coroutines.scopes
 
 import jakarta.inject.Inject
-import jakarta.inject.Named
-import jakarta.inject.Provider
 import jakarta.inject.Singleton
 import kotlinx.coroutines.CompletableJob
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.runBlocking
 import mu.KLogging
 import org.glassfish.hk2.api.Factory
-import org.glassfish.hk2.api.PerLookup
 import org.glassfish.hk2.utilities.binding.AbstractBinder
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
